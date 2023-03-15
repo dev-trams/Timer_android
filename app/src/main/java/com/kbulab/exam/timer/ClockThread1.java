09514690d0dcfc686210f490cb3fc9c467140918
trams <kppcgold@gmail.com>
2023-03-15 12:55:06 +0900

    android timer만들기

--- app/src/main/java/com/kbulab/exam/timer/ClockThread1.java
+++ app/src/main/java/com/kbulab/exam/timer/ClockThread1.java
@@ -9,9 +9,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class ClockThread1 extends Thread{
-    Context context;
-
-    TextView textView;
     Handler handler;
 
     public ClockThread1(Handler handler) {
@@ -22,13 +19,11 @@ public class ClockThread1 extends Thread{
     public void run() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
         while (true) {
-            Calendar calendar = Calendar.getInstance();
-            String time = dateFormat.format(calendar.getTime());
-
+            handler.sendEmptyMessage(0);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                Toast.makeText(context.getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                handler.sendEmptyMessage(1);
             }
         }
     }
